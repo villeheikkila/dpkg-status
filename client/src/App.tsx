485a309dc59e3b96cad05e260fce5fe666bfa61a
Ville Heikkilä <ville@heikkila.app>
2020-08-29 12:28:53 +0300

    feat: add search, sorting and simple stats

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -40,26 +40,42 @@ const useAxios = (baseUrl: string) => {
 const App = () => {
   const data: any = useAxios("http://localhost:2222/api/packages");
   const [search, setSearch] = useState("");
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {}, []);
 
   if (!data) return <div></div>;
+
+  const onKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      const value = inputRef?.current?.value || "";
+      setSearch(value);
+    }
+  };
+
+  const sortedData = data
+    .filter((e: any) => e.name.includes(search))
+    .sort((a: any, b: any) => a.name > b.name);
 
   return (
     <div>
       <Header>
         <Heading>dkpg-status</Heading>
-        <Input placeholder="Search..."></Input>
+        <Input placeholder="Search..." ref={inputRef} onKeyPress={onKeyPress} />
+        <Stats>
+          <p>Total packages: {data.length}</p>
+          <p>Currently showing: {sortedData.length}</p>
+        </Stats>
       </Header>
       <Page>
         <GridWrapper>
-          {data
-            .filter((e: any) => e.name.includes(search))
-            ?.map(({ id, name, description }: any) => (
-              <Package
-                key={`package-${id}`}
-                name={name}
-                description={description}
-              ></Package>
-            ))}
+          {sortedData?.map(({ id, name, description }: any) => (
+            <Package
+              key={`package-${id}`}
+              name={name}
+              description={description}
+            ></Package>
+          ))}
         </GridWrapper>
       </Page>
     </div>
@@ -90,11 +106,31 @@ const Heading = styled.h1`
   font-size: 48px;
 `;
 
+const Stats = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 30px;
+  margin-top: 10px;
+
+  > p {
+    margin: 0;
+  }
+`;
+
 const Input = styled.input`
   height: 40px;
-  width: 200px;
+  width: 300px;
   font-size: 14;
+  border: none;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+
+  ::selection {
+    outline: none;
+  }
+
+  ::placeholder {
+    padding-left: 10px;
+  }
 `;
 
 export default App;
