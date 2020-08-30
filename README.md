9761730c28e9645e8664014de61fc7a2b42ec939
Ville Heikkilä <ville@heikkila.app>
2020-08-31 01:17:40 +0300

    ci: fix things

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dpkg-status
+
+App that displays some key content from the dpkg status file in an easily understandable manner.
--- api/knexfile.js
+++ api/knexfile.js
@@ -7,6 +7,16 @@ module.exports = {
         migrations: {
             directory: __dirname + '/src/db/migrations',
         },
+        pool: {
+            min: 2,
+            max: 6,
+            createTimeoutMillis: 3000,
+            acquireTimeoutMillis: 30000,
+            idleTimeoutMillis: 30000,
+            reapIntervalMillis: 1000,
+            createRetryIntervalMillis: 100,
+            propagateCreateError: false,
+        },
     },
     production: {
         client: 'pg',
