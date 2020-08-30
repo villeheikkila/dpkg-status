46ff2280b67b7569c33bdc1850b098c147e44dc0
Ville Heikkilä <ville@heikkila.app>
2020-08-31 00:14:29 +0300

    ci: change env name for heroku

--- api/knexfile.js
+++ api/knexfile.js
@@ -3,14 +3,14 @@ require('dotenv').config({ path: '.env' });
 module.exports = {
     development: {
         client: 'pg',
-        connection: process.env.DATABASE_URL,
+        connection: process.env.DATABASE_URL_CUSTOM,
         migrations: {
             directory: __dirname + '/src/db/migrations',
         },
     },
     production: {
         client: 'pg',
-        connection: process.env.DATABASE_URL,
+        connection: process.env.DATABASE_URL_CUSTOM,
         migrations: {
             directory: __dirname + '/src/db/migrations',
         },
